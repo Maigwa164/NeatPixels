423728298e04548082f3d912106221bb5eb33d1f
David Maigwa <davidmaigwa164@gmail.com>
2025-02-24 18:48:22 +0300

    Add to cart coding challenge

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const btnCheck = document.querySelector(".btn");
+const btnCart = document.querySelector(".btn");
 
-btnCheck.addEventListener("click", function () {
-  const input = document.querySelector(".guess").value;
-  document.querySelector("body").style.backgroundColor = input;
+// btnCheck.addEventListener("click", function () {
+//   const input = document.querySelector(".guess").value;
+//   document.querySelector("body").style.backgroundColor = input;
+// });
+
+btnCart.addEventListener("click", function () {
+  document.querySelector("img").style.filter = "blur(5px)";
 });
