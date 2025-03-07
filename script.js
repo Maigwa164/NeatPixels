d4c25a0de9aac1ea2fb18f45e737f9434e77fe36
David Maigwa <davidmaigwa164@gmail.com>
2025-03-07 13:58:36 +0300

    relative & absolute positioning

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const btnCheck = document.querySelector(".btn");
 const btnCart = document.querySelector(".btn");
+const btnLike = document.querySelector(".like");
 
 // btnCheck.addEventListener("click", function () {
 //   const input = document.querySelector(".guess").value;
 //   document.querySelector("body").style.backgroundColor = input;
 // });
 
-btnCart.addEventListener("click", function () {
-  document.querySelector("img").style.filter = "blur(5px)";
+// btnCart.addEventListener("click", function () {
+//   document.querySelector("img").style.filter = "blur(5px)";
+// });
+
+btnLike.addEventListener("click", function () {
+  document.querySelector(".post-img").style.filter = "blur(10px)";
+  console.log("clicked");
 });
